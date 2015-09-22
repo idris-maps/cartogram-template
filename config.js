0246a98377a17fd8e6453b7ba9c33dd570638bfe
anders <anders@localhost.home>
2015-09-22 20:11:47 +0200

    first

---
+++ config.js
@@ -0,0 +1,20 @@
+var config = {
+	width: 960,
+	height: 600,
+	linkOnTopo: 'name',
+	pathToTopo: 'data/nuts2.json',
+	linkInCsv: 'name',
+	pathToCsv: 'data/pop.csv',
+	bbox: [-11.6,32.7,44.2,71.1],
+	scale: 37,
+	colorRange: ['#66c2a5','#ffffbf','#f46d43'],
+	valueInCsv: 'pop',
+	idFromTopo: 'name',
+	topoObject: 'nuts2',
+	fileName: 'NUTS2_pop',
+	stroke: {
+		width: 0.5,
+		opacity: 0.5,
+		color: 'black'
+	}
+}
